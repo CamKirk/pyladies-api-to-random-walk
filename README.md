67859f821d7d02dab4072d5c92f9fd15e750193e
camkirk <camden.kirkland@gmail.com>
2019-07-24 10:13:43 -0500

    finalizing for v1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Houston Pyladies: From API to Trading Bot
+
+Based on random walk code found at https://pythonforfinance.net/2016/11/28/monte-carlo-simulation-in-python/
+
+To run this code, you need to sign up for the Quandl API at quandl.com
--- libs/data.py
+++ libs/data.py
@@ -1,8 +1,15 @@
 import requests
 import pandas as pd
-from config import quandl_key
+from config import quandl_key # this file doesn't exist and you need to make it yourself!
 
 def get_data(ticker_symbol):
+    """User-defined function to GET data using the Quandl API
+        
+    
+    Returns:
+        dataframe () containing OHLC and Adj. OHLC info
+    
+    """
     url = f"https://www.quandl.com/api/v3/datasets/WIKI/{ticker_symbol}/data.json"
     params = {
         "api_key":quandl_key
